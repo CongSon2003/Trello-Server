c160cd5d769360f59c12c5c6b143d51571cdcaee
CongSon2003 <sonchuan1@gmail.com>
2025-02-10 00:28:58 +0700

    create_model

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -1,14 +1,30 @@
-import { formatter_slugify } from '~/utils/formatters'
-const createNew = (body) => new Promise ((resolve, reject) => {
+/* eslint-disable no-async-promise-executor */
+import { formatter_slugify } from '~/utils/formatters';
+import { boardModel } from '~/models/boardModel';
+import { StatusCodes } from 'http-status-codes';
+const createNew = (body) => new Promise (async (resolve, reject) => {
   try {
     // Đây là tầng xử lý dữ liệu của dự án
     console.log(body);
     console.log(formatter_slugify('Cong Son'));
-    // const new
-    resolve({
+
+    // Gọi tới model DB create Board
+    const response = await boardModel.createNew({
       ...body,
-      slug : formatter_slugify(body.title)
-    });
+      slug : formatter_slugify(body.title),
+      columnOrderIds : ['67a8e36cbcfeeafebaf7d7cd']
+    })
+
+    // Tìm Board sau khi create nếu không có thì return lỗi
+    const result = await boardModel.findOneById(response.insertedId)
+    if (!result) {
+      resolve({
+        statusCode : StatusCodes.NOT_FOUND,
+        message : "Error Created",
+        error : 1
+      })
+    }
+    resolve(result);
   } catch (error) {
     reject(error)
   }
---
+++ src/utils/validators.js
@@ -0,0 +1,2 @@
+export const validator_ObjectId = /^[0-9a-fA-F]{24}$/
+export const validator_ObjectId_message = 'Error ID (Pattern wrong)'
